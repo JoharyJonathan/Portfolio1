a49c4f9d07e625f2ab5ff6c8bb1f842376893c17
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-12-14 21:51:45 +0300

    Initial commit + tailwind dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline flex justify-center'>Test</h1>
+    </>
+  )
+}
+
+export default App
